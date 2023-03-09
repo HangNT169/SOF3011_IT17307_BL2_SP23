2c8acc6692ced35484bc352b750e15989f3b1627
HangNT169 <HangNT169@fpt.edu.vn>
2023-03-09 11:50:53 +0700

    B2: xử lý form

---
+++ src/main/java/com/poly/hangnt169/B2_OverviewJavaWeb/LoginController.java
@@ -0,0 +1,41 @@
+package com.poly.hangnt169.B2_OverviewJavaWeb;
+/**
+ * @author hangnt169
+ */
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginController", value = {"/login","/ket-qua"})
+public class LoginController extends HttpServlet {
+    // controller: Là tầng giao tiếp với client(view-jsp) thông qua liệu
+    // phương thức HTTP
+    // Ở Java 4, Java 5 chỉ làm việc với 2 phương thức :  GET, POST
+    // GET : Hiển thị dữ liệu
+    // POST: Xử lý dữ
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Truyền giá trị từ Servlet sang JSP
+        request.setAttribute("mess1", "HangNT169");
+        // Chuyển trang
+        request.getRequestDispatcher("form-login.jsp").forward(request,response);
+//        response.sendRedirect("/form-login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Lấy giá trị từ jsp về servlet
+        String username = request.getParameter("uname");
+        String password = request.getParameter("psw");
+
+        // Truyen du lieu jsp sang servlet
+        request.setAttribute("u1",username);
+        request.setAttribute("u2",password);
+
+        // Chuyen trang
+        request.getRequestDispatcher("hien-thi-ket-qua.jsp").forward(request,response);
+    }
+}
---
+++ src/main/webapp/form-login.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hangnt
+  Date: 09/03/2023
+  Time: 10:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${mess1}
+<form action="/ket-qua" method="post">
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+    </div>
+
+</form>
+</body>
+</html>
